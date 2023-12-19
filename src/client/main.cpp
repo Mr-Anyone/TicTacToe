afb5ce6908610d2abb1dd877f4ca6ace63311bc8
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2023-12-18 16:21:50 -0800

    Made the grid

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -19,7 +19,7 @@ void background_update(std::shared_ptr<sf::TcpSocket> socket, std::shared_ptr<Ga
     packet >> data; 
     game_world->updatePacket(data);
     std::cout << "I got data: " << data << std::endl;
-
+    printBoard(data.board); 
     while(true){
         sf::Packet packet;
         if(socket->receive(packet) != sf::Socket::Status::Done){
@@ -35,24 +35,65 @@ void background_update(std::shared_ptr<sf::TcpSocket> socket, std::shared_ptr<Ga
 void draw_board(const sf::Font& font, std::shared_ptr<GameWorld> game_world, sf::RenderWindow& window){
     // clearing the screen
     window.clear(sf::Color::White);
-
-    sf::Text text (font);
-    text.setString("Player: Cross"); 
-    text.setCharacterSize(25); 
-    text.setFillColor(sf::Color::Blue); 
-    
     NetworkPacket data = game_world->getPacket();
     
-    // drawing the font
-    switch(data.player){
-        case Player::CIRCLE:
-            text.setString("Player: Circle");
-            break;
-        case Player::CROSS: 
-            text.setString("Player: Cross");
-            break;
+    // displyaing player
+    {
+        sf::Text text (font);
+        text.setString("Player: Cross"); 
+        text.setCharacterSize(15); 
+        text.setFillColor(sf::Color::Blue); 
+        switch(data.player){
+            case Player::CIRCLE:
+                text.setString("Player: Circle");
+                break;
+            case Player::CROSS: 
+                text.setString("Player: Cross");
+                break;
+        }
+        window.draw(text);
     }
-    window.draw(text);
+
+    // displaying turn 
+    {
+        sf::Text text (font);
+        text.setPosition({0, 15}); 
+        text.setString("Turn: Cross"); 
+        text.setCharacterSize(15); 
+        text.setFillColor(sf::Color::Blue); 
+        switch(data.turn){
+            case Turn::CIRCLE_TURN: 
+                break; 
+            case Turn::CROSS_TURN:
+                break;
+        }
+        window.draw(text);
+    }
+
+
+    //drawing board
+    float width = window.getSize().x;
+    float height = window.getSize().y;
+    float width_dx = width / 3; 
+    float height_dx = height / 3;
+    // drawing vertical line
+    sf::RectangleShape vertical_line (sf::Vector2f(2.f, height));
+    vertical_line.setPosition({width_dx, 0}); 
+    vertical_line.setFillColor(sf::Color::Black); 
+    window.draw(vertical_line);
+    vertical_line.setPosition({width_dx * 2, 0}); 
+    vertical_line.setFillColor(sf::Color::Black); 
+    window.draw(vertical_line);
+    // drawing horizontal line 
+    sf::RectangleShape horizontal_line (sf::Vector2f(width, 2.0));
+    horizontal_line.setPosition({0, height_dx}); 
+    horizontal_line.setFillColor(sf::Color::Black); 
+    window.draw(horizontal_line);
+    horizontal_line.setPosition({0, height_dx * 2}); 
+    horizontal_line.setFillColor(sf::Color::Black); 
+    window.draw(horizontal_line);
+
+
 }
 
 int main(){
@@ -87,10 +128,10 @@ int main(){
         sf::Event event; 
         draw_board(font, game_world, window);
 
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
+            
         }
 
         window.display();
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -7,9 +7,21 @@
 
 constexpr long long TICK_RATE_MS = 500;
 
+void send_packet(sf::TcpSocket& socket, NetworkPacket& data){
+    sf::Packet packet;
+    packet << data;
+
+    if(socket.send(packet) != sf::Socket::Status::Done){
+        std::cout << "cannot send player packet" << std::endl; 
+        exit(-1);
+    }
+}
+
 int main(){
     sf::TcpListener listener;
     NetworkPacket global_state = makeNewNetworkPacket();
+    global_state.board[0][0] = BoardState::CIRCLE;
+
 
     if (listener.listen(10000) != sf::Socket::Status::Done){
         std::cout << "I cannot be binded to address" << std::endl;
@@ -25,35 +37,29 @@ int main(){
         return -1;
     }
     
-    {
-        sf::Packet packet;
-        global_state.player = Player::CIRCLE;
-        packet << global_state;
-
-        if(player1.send(packet) != sf::Socket::Status::Done){
-            std::cout << "cannot send player packet" << std::endl; 
-            return -1;
-        }
-    }
+    global_state.player = Player::CIRCLE;
+    send_packet(player1, global_state);
+  
 
     if(listener.accept(player2) != sf::Socket::Status::Done){
         std::cout << "cannot accpet listener" << std::endl; 
         return -1;
     }
 
-    {
-        sf::Packet packet;
-        global_state.player = Player::CROSS;
-        packet << global_state;
-        
-        if(player2.send(packet) != sf::Socket::Status::Done){
-            std::cout << "cannot send player packet" << std::endl; 
-            return -1;
-        }
-    }
+    global_state.player = Player::CROSS;
+    send_packet(player2, global_state);
+
+
     // enter loop
     while(true){
-        std::cout << "I've looped" << std::endl;
+        // waiting for packet based on turn
+
+        global_state.player = Player::CROSS;
+        send_packet(player2, global_state);
+        
+        global_state.player = Player::CROSS;
+        send_packet(player2, global_state);
+        // sending packet every TICK_RATE_MS
         std::this_thread::sleep_for(std::chrono::milliseconds(TICK_RATE_MS));    
     }
     return 0;
